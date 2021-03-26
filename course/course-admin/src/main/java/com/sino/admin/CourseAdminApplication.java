e5434a4f3ac26132e96cb906db89de3c781d9f27
xiaozj <904886481@qq.com>
2021-03-26 17:02:58 +0800

    original structure

---
+++ course/course-admin/src/main/java/com/sino/admin/CourseAdminApplication.java
@@ -0,0 +1,15 @@
+package com.sino.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaozj
+ */
+@SpringBootApplication
+public class CourseAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseAdminApplication.class, args);
+	}
+}
---
+++ course/course-admin/src/main/java/com/sino/admin/entity/CourseInfoEntity.java
@@ -0,0 +1,10 @@
+package com.sino.admin.entity;
+
+/**
+ * 课程信息的实体类
+ * @author xiaozj
+ */
+public class CourseInfoEntity {
+
+
+}
